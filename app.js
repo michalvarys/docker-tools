c4d730dfb2d7b4ca234bff75757a8473084d58d7
Kratomia vps <kratomia@michalvarys.eu>
2024-04-03 15:46:28 +0000

    fix env

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const axios = require("axios")
 const app = express()
-const PORT = 8001
+const PORT = 8000
 // Tell express to use body-parser's JSON parsing
 app.use(bodyParser.json())
 
